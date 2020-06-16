271cc50622ee97908addedbb82d58376f9b27ffd
wonthechan <packerstorm@gmail.com>
2020-06-16 13:10:32 +0900

    코멘트 관련부분 컨트롤러 복원

--- src/main/java/com/ssafy/happyhouse/controller/QnAController.java
+++ src/main/java/com/ssafy/happyhouse/controller/QnAController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.happyhouse.dto.CommentDto;
 import com.ssafy.happyhouse.dto.QnADto;
 import com.ssafy.happyhouse.service.QnAService;
 
@@ -78,4 +79,45 @@ public class QnAController {
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
+    
+    ////////////////////////// COMMENT /////////////////////////////////////
+    
+    @ApiOperation(value = "특정 게시글(no)의 모든 코멘트를 반환한다.", response = List.class)
+    @GetMapping("{no}/comments")
+    public ResponseEntity<List<CommentDto>> retrieveComment(@PathVariable int no) throws Exception {
+    	logger.debug("retrieveComment - 호출");
+    	return new ResponseEntity<List<CommentDto>>(service.retrieveComment(no), HttpStatus.OK);
+    }
+    
+    @ApiOperation(value = "새로운 코멘트를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+    @PostMapping("{no}/comments")
+    public ResponseEntity<String> writeComment(@RequestBody CommentDto dto) {
+    	logger.debug("writeComment - 호출");
+    	if (service.writeComment(dto)) {
+    		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+    	}
+    	return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+    }
+    
+    @ApiOperation(value = "코멘트 id에 해당하는 코멘트 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+    @PutMapping("{no}/comments/{cid}")
+    public ResponseEntity<String> updateComment(@RequestBody CommentDto dto) {
+    	logger.debug("updateComment - 호출");
+    	logger.debug("" + dto);
+    	
+    	if (service.updateComment(dto)) {
+    		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+    	}
+    	return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+    }
+    
+    @ApiOperation(value = "코멘트 no(id)에 해당하는 코멘트 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+    @DeleteMapping("{no}/comments/{cid}")
+    public ResponseEntity<String> deleteComment(@PathVariable int cid) {
+    	logger.debug("deleteComment - 호출");
+    	if (service.deleteComment(cid)) {
+    		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+    	}
+    	return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+    }
 }
